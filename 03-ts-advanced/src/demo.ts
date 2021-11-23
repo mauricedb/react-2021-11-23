e0fd68b8e11e13a15e03035874d7564a87583fe7
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-11-23 17:13:36 +0100

    Demo

--- 03-ts-advanced/src/demo.ts
+++ 03-ts-advanced/src/demo.ts
@@ -1,14 +1,15 @@
-const nerdyJokesUrl ="http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript";
+const nerdyJokesUrl =
+  'http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript';
 
 class Calculator {
   calculate(operation: string, x: number, y: number) {
     let result = NaN;
 
     switch (operation) {
-      case "Add":
+      case 'Add':
         result = this.add(x, y);
         break;
-      case "Subtract":
+      case 'Subtract':
         result = this.subtract(x, y);
         break;
     }
@@ -25,22 +26,54 @@ class Calculator {
   }
 }
 
-const x = document.getElementById("x") as HTMLInputElement;
-const y = document.getElementById("y") as HTMLInputElement;
-const add = document.getElementById("add");
-const subtract = document.getElementById("subtract");
-const result = document.getElementById("result");
+const x = document.getElementById('x') as HTMLInputElement;
+const y = document.getElementById('y') as HTMLInputElement;
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+const result = document.getElementById('result');
 
 const calculator = new Calculator();
 
-add.addEventListener("click", () => {
+add.addEventListener('click', () => {
   result.textContent = calculator
-    .calculate("Add", +x.value, +y.value)
+    .calculate('Add', +x.value, +y.value)
     .toString();
 });
 
-subtract.addEventListener("click", () => {
+subtract.addEventListener('click', () => {
   result.textContent = calculator
-    .calculate("Subtract", +x.value, +y.value)
+    .calculate('Subtract', +x.value, +y.value)
     .toString();
 });
+
+// const enum X {
+//   a = 'a',
+//   b = 'b',
+// }
+
+type X = 'a' | 'b';
+
+function useX(x: X, y: string) {
+  console.log(x);
+}
+
+useX('a', '');
+
+type PartofX = Partial<Calculator>;
+
+type Params = Parameters<typeof useX>;
+
+declare const _type: unique symbol;
+
+type Opaque<A, B> = A & {
+  readonly [_type]: B;
+};
+
+type Amount = Opaque<number, 'Amount'>;
+type Account = Opaque<number, 'Account'>;
+
+function sendMoney(account: Account, amount: Amount) {}
+
+const account: Account = 456 as Account;
+const amount: Amount = 123 as Amount;
+sendMoney(account, amount);
